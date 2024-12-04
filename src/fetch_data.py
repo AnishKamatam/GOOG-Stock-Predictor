5ce927b29905dee21b6701dd7045a1562ab5dcf0
AnishKamatam <anishkamatam22@gmail.com>
2024-12-04 09:12:33 -0800

    Update fetch_data.py

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -7,7 +7,6 @@ def fetch_google_data(start_date="2010-01-01", end_date="2024-12-01"):
 
     print(f"Fetching Google stock data from {start_date} to {end_date}...")
     
-    # Change stock symbol to 'GOOG' for Google stock
     google_data = yf.download("GOOG", start=start_date, end=end_date)
 
     file_path = os.path.join(data_folder, "google_stock_data.csv")
