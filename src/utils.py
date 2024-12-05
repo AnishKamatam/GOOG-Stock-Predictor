bb8fdb1b763d8b13c7fab03a5f297dc6536ef311
AnishKamatam <anishkamatam22@gmail.com>
2024-12-05 11:42:59 -0800

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,14 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+def preprocess_data(data, feature_column='Close', sequence_length=60):
+
+    data['Date'] = pd.to_datetime(data['Date'])
+    data.set_index('Date', inplace=True)
+    
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_data = scaler.fit_transform(data[feature_column].values.reshape(-1, 1))
 
 
 
